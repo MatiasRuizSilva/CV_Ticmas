65a7b29b8c3ba51e54c588a135d0e9399a35659a
matiasruizsilva <ruizsilvamatias@gmail.com>
2023-02-27 20:49:36 -0300

    se añadio icono sol para dark mode

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
+const boton = document.querySelector("#switch");
 
+const body = document.querySelector("body");
+
+boton.onclick = function () {
+    boton.classList.toggle("active");
+    body.classList.toggle("active");
+};
+
+boton.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    boton.classList.toggle("active");
+});
