5007423640a15407829df07763b3751a2ffb18cd
Matias Ruiz Silva <ruizsilvamatias@gmail.com>
2023-02-27 10:51:03 -0300

    2do commit

--- js.js
+++ js.js
@@ -1,15 +1,11 @@
 const colorClick = "#ff0000";
 
-
 const cambiacolor = function () {
     document.body.style.backgroundColor.value = colorClick;
 };
 
-document.getElementsByClassName("icono").addEvenListener("click", );
+document.getElementsByClassName("icono").addEvenListener("click");
 
 document
     .getElementsByClassName("contacto-icono")
     .addEvenListener("mouseover", cambiacolor);
-
-
-
