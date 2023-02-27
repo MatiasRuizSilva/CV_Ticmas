ab0acbdf07e01daad43cbf65ee99276edeafe522
Matias Ruiz Silva <ruizsilvamatias@gmail.com>
2023-02-27 12:09:52 -0300

    se agrega reset de css

--- js.js
+++ js.js
@@ -1,11 +1,1 @@
-const colorClick = "#ff0000";
 
-const cambiacolor = function () {
-    document.body.style.backgroundColor.value = colorClick;
-};
-
-document.getElementsByClassName("icono").addEvenListener("click");
-
-document
-    .getElementsByClassName("contacto-icono")
-    .addEvenListener("mouseover", cambiacolor);
